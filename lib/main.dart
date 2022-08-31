e2bc0dc39f0dd5facb18aaf50d972befbc555e93
Livingstone-Billy <72200615+Livingstone-Billy@users.noreply.github.com>
2022-08-31 15:35:53 +0300

    Update main.dart
    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,299 +1,201 @@
 import 'package:flutter/material.dart';
-import 'bmi_calculate.dart';
+import 'dart:math';
 
-void main() =>runApp(BMIApp());
+void main() =>runApp(const BMIApp());
 
 class BMIApp extends StatelessWidget{
+  const BMIApp({super.key});
+
   @override
   Widget build(BuildContext context){
-    return MaterialApp(
+    const appTitle = 'BMI Index Calculator';
+    return const MaterialApp(
+      title: appTitle,
       home: BMICalculator(),
     );
   }
 }
 
+class BMICalculator extends StatefulWidget{
+  const BMICalculator({super.key});
 
-class BMICalculator extends StatelessWidget{
+  @override
+  State<BMICalculator> createState() => _BMICalculatorState();
+}
 
-  List bmiInput = ["0", "1"];
-  
+class _BMICalculatorState extends State<BMICalculator>{
+
+  final heightController = TextEditingController();
+  final weightController = TextEditingController();
+
+  @override
+  void dispose(){
+    heightController.dispose();
+    weightController.dispose();
+
+    super.dispose();
+  }
+
+  double _getBMIIndex(String height, String weight){
+    double heightInMetres = double.parse(height) / 100;
+
+    double bmiIndex = double.parse(weight) / pow(heightInMetres, 2);
+
+    double index = double.parse((bmiIndex).toStringAsFixed(1));
+
+    return index;
+  }
+
+  String status = "BMI Index";
+
   @override
   Widget build(BuildContext context){
-    final heightTextField = Container(
-      margin: const EdgeInsets.only(top: 70),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const Text(
-            'Enter your height in cm',
-            style: TextStyle(
-              fontFamily: 'Roboto',
-              fontSize: 20,
-              fontWeight: FontWeight.w400,
-              // color: Colors.blue[400],
-            ),
-          ),
-          TextField(
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-            ),
-            keyboardType: TextInputType.number,
-            onChanged: (text){
-              bmiInput[0] = text;
-            },
-          ),
-        ],
-      ),
-    );
 
-    final weightTextField = Container(
-      margin: const EdgeInsets.only(top: 70),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          const Text(
-            'Enter your weight in kg',
-            style: TextStyle(
-              fontFamily: 'Roboto',
-              fontSize: 20,
-              fontWeight: FontWeight.w400,
-              // color: Colors.blue[400],
-            ),
-          ),
-          TextField(
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-            ),
-            keyboardType: TextInputType.number,
-            onChanged: (text){
-              bmiInput[1] = text;
+    var bmiTable = Container(
+      margin: const EdgeInsets.only(top: 20),
+      child: Card(
+          child: InkWell(
+            onTap: (){
+              debugPrint("BMI Score Tapped");
             },
-          ),
-        ],
-      ),
-    );
-    return MaterialApp(
-      title: 'BMI Calculator',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('BMI Calculator'),
-          backgroundColor: Colors.green[800],
-        ),
-        body: SingleChildScrollView(
-          child: Center(
             child: SizedBox(
-              width: 240,
-              child: Column(
+              height: 150,
+              width: 350,
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  heightTextField,
-                  weightTextField,
-                  Container(
-                    margin: const EdgeInsets.only(top: 80),
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.green[600],
-                        minimumSize: const Size(200, 70),
-                      ),
-                      onPressed: (){
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context)=>BMITableInfo())
-                        );
-                      },
-                      child: const Text(
-                          'Compute BMI',
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: const [
+                      Text(
+                        "UnderWeight",
                         style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w800,
+                          color: Colors.greenAccent
                         ),
                       ),
-                    )
+                      Text(
+                        "NormalWeight",
+                        style: TextStyle(
+                            color: Colors.blueAccent
+                        ),
+                      ),
+                      Text(
+                        "OverWeight",
+                        style: TextStyle(
+                            color: Colors.redAccent
+                        ),
+                      ),
+                      Text(
+                        "Obese",
+                        style: TextStyle(
+                            color: Colors.red
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: const [
+                      Text(
+                        "18.5",
+                        style: TextStyle(
+                          color: Colors.greenAccent
+                        ),
+                      ),
+                      Text(
+                        "18.5 - 24.9",
+                        style: TextStyle(
+                            color: Colors.blueAccent
+                        ),
+                      ),
+                      Text(
+                        "25 - 29.9",
+                        style: TextStyle(
+                            color: Colors.redAccent
+                        ),
+                      ),
+                      Text(
+                        "30",
+                        style: TextStyle(
+                            color: Colors.red
+                        ),
+                      ),
+                    ],
                   )
                 ],
               ),
             ),
-          ),
-        ),
-      )
+          )
+      ),
     );
-  }
-}
 
-class BMITableInfo extends StatelessWidget{
-
-  GenerateBMI calculator = GenerateBMI();
-  final BMICalculator _calculator = BMICalculator();
-
-  @override
-  Widget build(BuildContext context){
-    var decTexStyle = const TextStyle(
-      fontWeight: FontWeight.w500,
-      fontFamily: 'Roboto',
-      fontSize: 18,
-    );
-    var bmiInfoSection = Container(
-      margin: const EdgeInsets.only(top: 40),
-      child: Column(
-        children: [
-           const Text(
-            'Your BMI Score',
-            style: TextStyle(
-              color: Colors.blueAccent,
-              fontWeight: FontWeight.w500,
-              fontSize: 18
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BMI Index Calculator'),
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+            child: TextFormField(
+              controller: heightController,
+              autofocus: true,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Enter your height in cm'
+              ),
             ),
           ),
-          SizedBox(
-            width: 200,
-            height: 70,
-            child: Card(
-              color: Colors.green,
-              child: Center(
-                child: Text(
-                  '${calculator.calculateBMI()}',
-                  style: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.white,
-                  ),
-                ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+            child:  TextFormField(
+              controller: weightController,
+              autofocus: true,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Enter your weight in kg'
               ),
             ),
           )
         ],
       ),
-    );
-
-    var bmiTable = Container(
-      margin: const EdgeInsets.only(top: 80),
-      child: Card(
-        child: InkWell(
-          onTap: (){
-            debugPrint("BMI Score Tapped");
-          },
-          child: SizedBox(
-            height: 150,
-            width: 350,
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      "UnderWeight",
-                      style: decTexStyle,
-                    ),
-                    const Text(
-                      "NormalWeight",
-                      style: TextStyle(
-                          fontWeight: FontWeight.w500,
-                          fontFamily: 'Roboto',
-                          fontSize: 18,
-                          color: Colors.blueAccent
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          showDialog(
+            context: context,
+            builder: (context){
+              return AlertDialog(
+                actions: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                          'Your BMI index is ${_getBMIIndex(heightController.text,
+                              weightController.text)}'
                       ),
-                    ),
-                    const Text(
-                      "OverWeight",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontFamily: 'Roboto',
-                        fontSize: 18,
-                        color: Colors.red
-                      ),
-                    ),
-                    const Text(
-                      "Obese",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontFamily: 'Roboto',
-                        fontSize: 18,
-                        color: Colors.red
-                      ),
-                    ),
-                  ],
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      "18.5",
-                      style: decTexStyle,
-                    ),
-                    const Text(
-                      "18.5 - 24.9",
-                      style: TextStyle(
-                          fontWeight: FontWeight.w500,
-                          fontFamily: 'Roboto',
-                          fontSize: 18,
-                          color: Colors.blueAccent
-                      ),
-                    ),
-                    const Text(
-                      "25 - 29.9",
-                      style: TextStyle(
-                          fontWeight: FontWeight.w500,
-                          fontFamily: 'Roboto',
-                          fontSize: 18,
-                          color: Colors.red
-                      ),
-                    ),
-                    const Text(
-                      "30",
-                      style: TextStyle(
-                          fontWeight: FontWeight.w500,
-                          fontFamily: 'Roboto',
-                          fontSize: 18,
-                          color: Colors.red
-                      ),
-                    ),
-                  ],
-                )
-              ],
-            ),
-          ),
-        )
-      ),
-    );
-
-    var backButton = Container(
-      margin: const EdgeInsets.only(top: 98),
-      child: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          primary: Colors.green,
-          minimumSize: const Size(150, 60)
-        ),
-        onPressed: ()=> Navigator.pop(context),
-        child: Text(
-          'Back',
-          style: decTexStyle,
-        ),
-      ),
-    );
-
-    return MaterialApp(
-      title: 'BMI Result Page',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("BMI Calculator"),
-          backgroundColor: Colors.green,
-        ),
-        body: SingleChildScrollView(
-          child: Center(
-            child: Column(
-              children: [
-                bmiInfoSection,
-                bmiTable,
-                backButton
-              ],
-            ),
-          ),
-        ),
+                      bmiTable,
+                      ElevatedButton(
+                        onPressed: (){
+                          Navigator.pop(context);
+                        },
+                        child: const Text('Close'),
+                      )
+                    ],
+                  )
+                ],
+              );
+            }
+          );
+        },
+        tooltip: 'Compute BMI',
+        child: const Text('BMI'),
       ),
     );
   }
